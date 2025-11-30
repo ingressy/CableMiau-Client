8de987585925fdd4a2b6ac830dc707c774910074
Noora Liv <j.czinzoll@ingressy.de>
2025-11-30 11:03:52 +0100

    Create login.java

---
+++ GUI/login.java
@@ -0,0 +1,54 @@
+package GUI;
+import ACTIONS.LoginActions;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class login {
+    public static void login_gui() {
+        JFrame frame = new JFrame("NetCat - Login");
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+
+        JCheckBox checkBox = new JCheckBox("keep log in data?");
+        JCheckBox passwordBox = new JCheckBox("show password?");
+        char defaultEcho = passwordField.getEchoChar();
+
+        JButton loginButton = new JButton("Log in");
+
+        frame.setLayout(new GridLayout(7,1));
+        frame.add(usernameLabel);
+        frame.add(usernameField);
+
+        frame.add(passwordLabel);
+        frame.add(passwordField);
+        frame.add(passwordBox);
+
+        frame.add(checkBox);
+        frame.add(loginButton);
+
+        frame.getRootPane().setDefaultButton(loginButton);
+        frame.setSize(400,200);
+        frame.setVisible(true);
+
+        //logo loading - use normal logo by failed
+        try {
+            frame.setIconImage(ImageIO.read(new File("src/GUI/ethernet.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        loginButton.addActionListener(_ -> LoginActions.loginAction(usernameField, passwordField)
+        );
+        passwordBox.addActionListener(_ -> LoginActions.showPasswordAction(passwordBox, passwordField, defaultEcho));
+    }
+}
